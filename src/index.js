fdea8f4f12cf2765c963ce991678a643032bd10c
Arkar Chan Myae <arkarcham20@gmail.com>
2023-07-08 18:12:37 +0630

    React Dev-Profile

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        <SkillLists />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img src="./avatar.jpg" className="avatar" alt="Arkar Chan Myae" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Arkar Chan Myae</h1>
+      <p>
+        I'm trying to be Full-Stack Web Developer.When not coding, I'd like to
+        play games and listen to some good songs. And I love to solve error even
+        it is boring and complicated stuff but I enjoy doing it.Fun Fact: do not
+        ask how my code works. Just Kidding!. I'd love to understand how codes
+        work , also share with others about tech and new stuff whenever I can
+        get my hands on or not.
+      </p>
+    </div>
+  );
+}
+
+function SkillLists() {
+  return (
+    <div className="skill-list">
+      <Skill skill="HTML + CSS" emoji="👍" color="#123456" />
+      <Skill skill="JAVASCRIPT" emoji="👍" color="orangered" />
+      <Skill skill="Tailwind" emoji="👶" color="steelblue" />
+      <Skill skill="React" emoji="👶" color="skyblue" />
+      <Skill skill="PHP" emoji="👶" color="#AEB2D5" />
+      <Skill skill="MYSQL" emoji="👍" color="#00758f" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
